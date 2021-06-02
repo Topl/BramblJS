4a725daed240ba6d91ea04f2866c5cb3b8355ada
Sterling Wells <s.wells@topl.me>
2021-06-02 09:39:36 -0700

    Added ability to read keypairs from file to KeyManager

--- src/modules/KeyManager.js
+++ src/modules/KeyManager.js
@@ -84,14 +84,8 @@ class KeyManager {
        */
 
       const initKeyStorage = (keyStorage, password) => {
-        this.#address = keyStorage.address;
         this.#isLocked = false;
-        this.#password = password;
-        this.#keyStorage = keyStorage;
-
-        if (this.#address) {
-          [this.#sk, this.#pk] = recover(password, keyStorage, this.constants.scrypt);
-        }
+        this.setKeyStorage(keyStorage, password);
       };
 
       /**
@@ -103,40 +97,6 @@ class KeyManager {
       const generateKey = (password) => {
         // this will create a new curve25519 key pair and dump to an encrypted format
         initKeyStorage(dump(password, create(this.constants), this.constants), password);
-      };
-
-      /**
-       * Imports key data object from keystore JSON file.
-       * @param {string} filepath the filepath of the keystore JSON
-       * @param {string} password the password for encrypting/decrypting * the keyfile
-       * @returns {object} returns the keyStorage used in the KeyManager
-       */
-      const importFromFile = (filepath, password) => {
-        const keyStorage = JSON.parse(fs.readFileSync(filepath));
-
-        // check if address is valid and has a valid network
-        if (keyStorage.address) {
-          // determine prefix and set networkPrefix
-          const prefixResult = utils.getAddressNetwork(keyStorage.address);
-          if (prefixResult.success) {
-            this.#networkPrefix = prefixResult.networkPrefix;
-          } else {
-            throw new Error(prefixResult.error);
-          }
-
-          // validate address
-          const validationResult = utils.validateAddressesByNetwork(this.networkPrefix, keyStorage.address);
-          if (!validationResult.success) {
-            throw new Error("Invalid Addresses::" +
-              " Network Type: <" + this.networkPrefix + ">" +
-              " Invalid Addresses: <" + validationResult.invalidAddresses + ">" +
-              " Invalid Checksums: <" + validationResult.invalidChecksums + ">");
-          }
-
-          initKeyStorage(keyStorage, password);
-        } else {
-          throw new Error("No address found in key");
-        }
       };
 
       // initialize variables
@@ -157,7 +117,7 @@ class KeyManager {
       // load in keyfile if a path was given, or default to generating a new key
       if (params.keyPath) {
         try {
-          importFromFile(params.keyPath, params.password);
+          this.importFromFile(params.keyPath, params.password);
         } catch (err) {
           throw new Error("Error importing keyfile - " + err);
         }
@@ -190,160 +150,222 @@ class KeyManager {
     };
 
     /* ------------------------------ Public methods -------------------------------- */
+
     /**
+       * Imports the keyfile data object
+       * @param {object} keyStorage: The JS object representing the encrypted keyfile
+       * @param {object} password: The password to unlock the keyfile
+       * @returns {object} returns the keyStorage used in the KeyManager
+       */
+    importKeyPair(keyStorage, password) {
+      // check if address is valid and has a valid network
+      if (keyStorage.address) {
+        // determine prefix and set networkPrefix
+        const prefixResult = utils.getAddressNetwork(keyStorage.address);
+        if (prefixResult.success) {
+          this.#networkPrefix = prefixResult.networkPrefix;
+        } else {
+          throw new Error(prefixResult.error);
+        }
+
+        // validate address
+        const validationResult = utils.validateAddressesByNetwork(this.networkPrefix, keyStorage.address);
+        if (!validationResult.success) {
+          throw new Error("Invalid Addresses::" +
+                " Network Type: <" + this.networkPrefix + ">" +
+                " Invalid Addresses: <" + validationResult.invalidAddresses + ">" +
+                " Invalid Checksums: <" + validationResult.invalidChecksums + ">");
+        }
+
+        this.setKeyStorage(keyStorage, password);
+      } else {
+        throw new Error("No address found in key");
+      }
+    };
+
+      /**
+         * Imports key data object from keystore JSON file.
+         * @param {string} filepath the filepath of the keystore JSON
+         * @param {string} password the password for encrypting/decrypting * the keyfile
+         * @returns {object} returns the keyStorage used in the KeyManager
+         */
+      importFromFile = (filepath, password) => {
+        const keyStorage = JSON.parse(fs.readFileSync(filepath));
+
+        return this.importKeyPair(keyStorage, password);
+      };
+      /**
      * Getter function to retrieve key storage in the Bifrost compatible format
      * @function GetKeyStorage
      * @memberof KeyManager
      * @returns {object} returns value of private var keyStorage
      */
-    getKeyStorage() {
-      if (this.#isLocked) throw new Error("Key manager is currently locked. Please unlock and try again.");
-      if (!this.#pk) throw new Error("A key must be initialized before using this key manager");
-      return this.#keyStorage;
-    }
+      getKeyStorage() {
+        if (this.#isLocked) throw new Error("Key manager is currently locked. Please unlock and try again.");
+        if (!this.#pk) throw new Error("A key must be initialized before using this key manager");
+        return this.#keyStorage;
+      }
 
-    /**
+      /**
+       * Setter function to input keyStorage in the Bifrost compatible format
+       * @param {object} keyStorage - The keyStorage object that the keyManager will use to store the keys for a particular address.
+       * @param {string} password for encrypting (decrypting) the keyfile
+       * @function setKeyStorage
+       * @memberof KeyManager
+       * @returns {object};
+       */
+      setKeyStorage(keyStorage, password) {
+        if (this.#isLocked) throw new Error("Key manager is currently locked. Please unlock and try again.");
+        this.#address = keyStorage.address;
+        this.#password = password;
+        this.#keyStorage = keyStorage;
+        if (this.#address) {
+          [this.#sk, this.#pk] = recover(password, keyStorage, this.constants.scrypt);
+        }
+      }
+
+      /**
      * Set the key manager to locked so that the private key may not be decrypted
      * @memberof KeyManager
      * @returns {void}
      */
-    lockKey() {
-      this.#isLocked = true;
-    }
+      lockKey() {
+        this.#isLocked = true;
+      }
 
-    /**
+      /**
      * Getter for private property #isLocked
      * @memberof KeyManager
      * @returns {boolean} value of #isLocked
      */
-    get isLocked() {
-      return this.#isLocked;
-    }
+      get isLocked() {
+        return this.#isLocked;
+      }
 
-    /**
+      /**
      * Setter for private property #isLocked
      * @memberof KeyManager
      * @param {any} args ignored, only necessary for setter
      * @returns {void} Error is thrown to protect private variable
      */
-    set isLocked(args) {
-      throw new Error("Invalid private variable access, use lockKey() instead.");
-    }
+      set isLocked(args) {
+        throw new Error("Invalid private variable access, use lockKey() instead.");
+      }
 
-    /**
+      /**
      * Getter for private property #pk
      * @memberof KeyManager
      * @returns {string} value of #pk (public key string)
      */
-    get pk() {
-      return this.#pk;
-    }
+      get pk() {
+        return this.#pk;
+      }
 
-    /**
+      /**
      * Setter for private property #pk
      * @memberof KeyManager
      * @param {any} args ignored, only necessary for setter
      * @returns {void} Error is thrown to protect private variable
      */
-    set pk(args) {
-      throw new Error("Invalid private variable access, instantiate a new KeyManager instead.");
-    }
+      set pk(args) {
+        throw new Error("Invalid private variable access, instantiate a new KeyManager instead.");
+      }
 
-    /**
+      /**
      * Getter for private property #address
      * @memberof KeyManager
      * @param {any} args ignored, only necessary for setter
      * @returns {void} Error is thrown to protect private variable
      */
-    get address() {
-      return this.#address;
-    }
+      get address() {
+        return this.#address;
+      }
 
-    /**
+      /**
      * Setter for private property #address
      * @memberof KeyManager
      * @param {any} args ignored, only necessary for setter
      * @returns {void} Error is thrown to protect private variable
      */
-    set address(args) {
-      throw new Error("Invalid private variable access, instantiate a new KeyManager instead.");
-    }
+      set address(args) {
+        throw new Error("Invalid private variable access, instantiate a new KeyManager instead.");
+      }
 
-    /**
+      /**
      * Getter for private property #networkPrefix
      * @memberof KeyManager
      * @returns {string} value of #networkPrefix
      */
-    get networkPrefix() {
-      return this.#networkPrefix;
-    }
+      get networkPrefix() {
+        return this.#networkPrefix;
+      }
 
-    /**
+      /**
      * Setter for private property #isLocked
      * @memberof KeyManager
      * @param {any} args ignored, only necessary for setter
      * @returns {void} Error is thrown to protect private variable
      */
-    set networkPrefix(args) {
-      throw new Error("Invalid private variable access.");
-    }
+      set networkPrefix(args) {
+        throw new Error("Invalid private variable access.");
+      }
 
-    /**
+      /**
      * Unlock the key manager to be used in transactions
      * @param {string} password encryption password for accessing the keystorage object
      * @memberof KeyManager
      * @returns {void}
      */
-    unlockKey(password) {
-      if (!this.#isLocked) throw new Error("The key is already unlocked");
-      if (password !== this.#password) throw new Error("Invalid password");
-      this.#isLocked = false;
-    }
+      unlockKey(password) {
+        if (!this.#isLocked) throw new Error("The key is already unlocked");
+        if (password !== this.#password) throw new Error("Invalid password");
+        this.#isLocked = false;
+      }
 
-    /**
+      /**
      * Generate the signature of a message using the provided private key
      * @param {string} message Message to sign (utf-8 encoded)
      * @memberof KeyManager
      * @returns {Uint8Array} signature
      */
-    sign(message) {
-      if (this.#isLocked) throw new Error("The key is currently locked. Please unlock and try again.");
-      if (!this.#sk) throw new Error("A key must be initialized before using this key manager");
-      if (!message || message.constructor !== String) throw new Error("Invalid message provided as argument.");
+      sign(message) {
+        if (this.#isLocked) throw new Error("The key is currently locked. Please unlock and try again.");
+        if (!this.#sk) throw new Error("A key must be initialized before using this key manager");
+        if (!message || message.constructor !== String) throw new Error("Invalid message provided as argument.");
 
-      return curve25519.sign(str2buf(this.#sk), str2buf(message, "base58"), crypto.randomBytes(64));
-    }
+        return curve25519.sign(str2buf(this.#sk), str2buf(message, "base58"), crypto.randomBytes(64));
+      }
 
-    /**
+      /**
      * Export formatted JSON to keystore file.
      * @param {string=} _keyPath Path to keystore folder (default: ".keyfiles")
      * @returns {string} JSON filename
      * @memberof KeyManager
      */
-    exportToFile(_keyPath) {
-      if (this.#isLocked) throw new Error("The key is currently locked. Please unlock and try again.");
-      if (!this.#pk) throw new Error("A key must be initialized before using this key manager");
-      if (_keyPath && _keyPath.constructor !== String) throw new Error("Invalid keypath provided as argument.");
+      exportToFile(_keyPath) {
+        if (this.#isLocked) throw new Error("The key is currently locked. Please unlock and try again.");
+        if (!this.#pk) throw new Error("A key must be initialized before using this key manager");
+        if (_keyPath && _keyPath.constructor !== String) throw new Error("Invalid keypath provided as argument.");
 
-      const keyPath = _keyPath || ".keyfiles";
-      const outfile = generateKeystoreFilename(this.#pk);
-      const json = JSON.stringify(this.getKeyStorage());
-      const outpath = path.join(keyPath, outfile);
+        const keyPath = _keyPath || ".keyfiles";
+        const outfile = generateKeystoreFilename(this.#pk);
+        const json = JSON.stringify(this.getKeyStorage());
+        const outpath = path.join(keyPath, outfile);
 
-      // write file and return outpath if successful or throw error
-      try {
+        // write file and return outpath if successful or throw error
+        try {
         // create default directory if it doesn't exist
-        if (keyPath === ".keyfiles" && !fs.existsSync(keyPath)) {
-          fs.mkdirSync(keyPath);
+          if (keyPath === ".keyfiles" && !fs.existsSync(keyPath)) {
+            fs.mkdirSync(keyPath);
+          }
+          // write file
+          fs.writeFileSync(outpath, json);
+        } catch (error) {
+          throw new Error("Error exporting to file." + error);
         }
-        // write file
-        fs.writeFileSync(outpath, json);
-      } catch (error) {
-        throw new Error("Error exporting to file." + error);
-      }
 
-      return outpath;
-    }
+        return outpath;
+      }
 };
 
 /* -------------------------------------------------------------------------- */
